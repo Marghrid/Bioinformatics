f594553bb3f3692da1ab1f3c65f3be456b95f641
Ricardo Brancas <ricardo.brancas@tecnico.ulisboa.pt>
2019-12-07 17:59:09 +0000

    Update

--- paper-report/vie2msf.py
+++ paper-report/vie2msf.py
@@ -3,10 +3,26 @@ import glob
 import os
 import subprocess
 
-files = glob.glob('**/*.vie', recursive=True)
-print(files)
+# files = glob.glob('**/bralibase/**/structural/*.fasta', recursive=True)
+# print(files)
+#
+# for file in files:
+#     filename, file_extension = os.path.splitext(file)
+#
+#     subprocess.Popen(['seqret', file, '-osf', 'msf', '-outseq', filename + '.msf'])
 
-for file in files:
-    filename, file_extension = os.path.splitext(file)
+# files = glob.glob('**/bralibase/**/structural/*.fa', recursive=True)
+# print(files)
+#
+# for file in files:
+#     filename, file_extension = os.path.splitext(file)
+#
+#     subprocess.Popen(['seqret', file, '-osf', 'msf', '-outseq', filename + '.msf'])
 
-    subprocess.Popen(['seqret', file, '-osf', 'msf', '-outseq', filename + '.msf'])
+# files = glob.glob('**/bralibase/**/unaligned/*.fasta', recursive=True)
+# print(files)
+#
+# for file in files:
+#     filename, file_extension = os.path.splitext(file)
+#
+#     subprocess.Popen(['mv', file, os.path.split(os.path.split(filename)[0])[0] + '/' + os.path.split(filename)[1] + '.tfa'])
